7f13168647e68707d66dd770196efe701f074f6c
Mayank Kushwaha <mayank.kushwaha07@gmail.com>
2018-06-06 11:17:33 +0530

    folder structure and gallery.js file created

---
+++ src/js/components/gallery.js
@@ -0,0 +1,17 @@
+import React ,{Component} from'react';
+
+class Gallery extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Shorty u will view the Gallery </h1>
+                </div>
+        )
+    }
+}
+
+export default Gallery;
